655ab4da5fa1002447038599f2ccd613d5f60d4e
sam-echoapps <sam@echoapps360.com>
2024-06-05 20:32:31 +0530

    Bulk Reassignment Log Commit

--- src/js/viewModels/manageUsers.js
+++ src/js/viewModels/manageUsers.js
@@ -597,6 +597,7 @@ define(['ojs/ojcore',"knockout","jquery","appController", "ojs/ojpagingdataprovi
                             officeId: self.officeId(),
                             counsilorId: self.counsilor(),
                             studentId: studentIds,
+                            userId: sessionStorage.getItem("userId")
                         }),
                         dataType: 'json',
                         error: function (xhr, textStatus, errorThrown) {
